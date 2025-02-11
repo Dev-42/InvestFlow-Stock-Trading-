7cf63cc5d69addfab7ffaf1561dc2bfe5931acdd
Dev-42 <devbhattacharya42@gmail.com>
2025-02-11 15:51:03 +0530

    Retrive all the stocks done.

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const { resolve } = require('path');
+const {getAllStocks} = require('./controllers/index')
 
 const app = express();
 const port = 3010;
 
-app.use(express.static('static'));
-
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+app.get('/stocks',(req,res) => {
+  try{
+    const response = getAllStocks()
+    if(response.length === 0){
+      return res.status(404).json({message : "Stocks were not present"})
+    }
+    return res.status(200).json(response)
+  }catch(error){
+    return res.status(500).json({error:error.message})
+  }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
